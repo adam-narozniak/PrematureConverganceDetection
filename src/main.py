2ea6ca3fabc8ae63606dd4c2bc4bbb1f2667c45f
IG <01147613@pw.edu.pl>
2021-03-29 19:10:18 +0200

    Adding lib that allows to run CEC2017 func from C in Py

--- src/main.py
+++ src/main.py
@@ -1,22 +1,32 @@
 from src.cec17_test_func.cec17_functions import cec17_test_func
 
 
-# minimalization
+class Population:
+    def __init__(self, size_of_population, number_of_features):
+        size = size_of_population
+        n_feature = number_of_features
+
+
+# co dalej?
+
+# minimization
 def holland(population, iterations, mutation_probability, crossover_probability):
     if population.ndims != 2:
         raise Exception(f"Wrong format of population")
+
     population_length = population.shape[0]
     n_features = population.shape[1]
+
     if population_length < 1:
         raise Exception(f"Population should be positive number, not {population_length}")
     if n_features < 1:
         raise Exception(f"Number of features should be positive number, not {population_length}")
 
     scores = evaluate(population, cost_function)
-    best = find_best(population, scores)
+    best = find_best(scores)
 
     for iteration in range(iterations):
-        '''if stop_criterion_satisfied(population, scores):
+        if stop_criterion_satisfied(population, scores):
             break
         # during lectures third parameter was passed - length, no use of that
         children = reproduce(population, scores)
@@ -30,34 +40,35 @@ def holland(population, iterations, mutation_probability, crossover_probability)
 
         best = best if best < iteration_best else iteration_best
         population, scores = succession(population, mutants_and_crossovered, scores, scores_mutants)
-'''
+
     # and what it returns???
 
 
-def evaluate(population, cost_function):
-    # x: Solution vector
-    x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    # nx: Number of dimensions
-    nx = 10
-    # mx: Number of objective functions
-    mx = 1
-    # func_num: Function number
-    func_num = 1
-    # Pointer for the calculated fitness
-    f = [0]
-    cec17_test_func(x, f, nx, mx, 10)
-    print(f[0])
+def evaluate(population, cec2017_func_number):
+    scores = []
+    dimension = population.shape[1]
+    mx = 1  # number of function to calculate
+    for individual in population:
+        placeholder_for_score = [0]
+        cec17_test_func(individual, placeholder_for_score, dimension,
+                        mx, cec2017_func_number)
+        scores.append(placeholder_for_score[0])
+    return scores
 
 
 def find_best(scores):
-    pass
+    index_of_best_individual = 0
+    for i in range(scores):
+        if scores[i] < scores[index_of_best_individual]:
+            index_of_best_individual = i
+    return index_of_best_individual
 
 
 def reproduce(population, scores):
     pass
 
 
-def succession(population , mutants, scores, scores_mutants):
+def succession(population, mutants, scores, scores_mutants):
     pass
 
 
@@ -65,7 +76,14 @@ def cost_function():
     pass
 
 
+def stop_criterion_satisfied(population, scores):
+    pass
+
+
+def mutate_and_crossover(children, mutation_probability, crossover_probability):
+    pass
+
+
 if __name__ == '__main__':
     print("Hello word")
     evaluate(0, 0)
-

8e9c1e132ca4edeff0ea1baff7c8ef4c2d978d50
IG <01147613@pw.edu.pl>
2021-03-29 13:46:58 +0200

    Adding lib that allows to run CEC2017 func from C in Py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
-#minimalization
+from src.cec17_test_func.cec17_functions import cec17_test_func
+
+
+# minimalization
 def holland(population, iterations, mutation_probability, crossover_probability):
     if population.ndims != 2:
         raise Exception(f"Wrong format of population")
     population_length = population.shape[0]
     n_features = population.shape[1]
-    if  population_length < 1:
+    if population_length < 1:
         raise Exception(f"Population should be positive number, not {population_length}")
     if n_features < 1:
         raise Exception(f"Number of features should be positive number, not {population_length}")
@@ -13,7 +16,7 @@ def holland(population, iterations, mutation_probability, crossover_probability)
     best = find_best(population, scores)
 
     for iteration in range(iterations):
-        if stop_criterion_satisfied(population, scores):
+        '''if stop_criterion_satisfied(population, scores):
             break
         # during lectures third parameter was passed - length, no use of that
         children = reproduce(population, scores)
@@ -27,21 +30,42 @@ def holland(population, iterations, mutation_probability, crossover_probability)
 
         best = best if best < iteration_best else iteration_best
         population, scores = succession(population, mutants_and_crossovered, scores, scores_mutants)
-
+'''
     # and what it returns???
 
 
 def evaluate(population, cost_function):
+    # x: Solution vector
+    x = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # nx: Number of dimensions
+    nx = 10
+    # mx: Number of objective functions
+    mx = 1
+    # func_num: Function number
+    func_num = 1
+    # Pointer for the calculated fitness
+    f = [0]
+    cec17_test_func(x, f, nx, mx, 10)
+    print(f[0])
+
 
 def find_best(scores):
+    pass
+
 
 def reproduce(population, scores):
+    pass
+
 
 def succession(population , mutants, scores, scores_mutants):
+    pass
+
 
 def cost_function():
+    pass
 
 
 if __name__ == '__main__':
-    holland()
+    print("Hello word")
+    evaluate(0, 0)
 

36b8d907435d909d388cc2a46ab2cb9cc05f8a09
Adam <narozniak.adam4@gmail.com>
2021-04-18 19:14:14 +0200

    vector implementation added

--- src/crossovers.py
+++ src/crossovers.py
@@ -2,21 +2,22 @@ import numpy as np
 import math as m
 
 
-def crossover(population, crossover_probability, rng, feature_crossover_probability=0.5):
+def crossover(population, crossover_probability, rng):
+    """Perform crossover operation (pl. krzyżownie jednopunktowe)"""
     population_size = population.shape[0]
     n_features = population.shape[1]
-    # crossover (pl.krzyżowanie równomierne)
     crossover_uniform = rng.uniform(0, 1, population_size)
 
-    # polaczmy je w pary
     for i in range(m.floor(population_size / 2)):
         if crossover_probability > crossover_uniform[i]:
             genome_crossover_point = np.random.randint(1, high=n_features)
-            for j in range(0, genome_crossover_point):
-                swapper = population[i][j]
-                population[i][j] = population[population_size - i - 1][j]
-                population[population_size - i - 1][j] = swapper
-
+            temp = population[i][:genome_crossover_point]
+            population[i][:genome_crossover_point] = population[population_size-i-1][:genome_crossover_point]
+            population[population_size-i-1][:genome_crossover_point] = temp
+            # for j in range(0, genome_crossover_point):
+            #     swapper = population[i][j]
+            #     population[i][j] = population[population_size - i - 1][j]
+            #     population[population_size - i - 1][j] = swapper
     return population
 
 # testy, potem do usuniecia

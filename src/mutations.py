a3959662da85d6953867b562d93ad004ecef7103
Adam <narozniak.adam4@gmail.com>
2021-04-20 11:52:39 +0200

    Data clipping added to given range -100, 100

--- src/mutations.py
+++ src/mutations.py
@@ -1,4 +1,14 @@
 def mutate(population, mutation_probability, mutation_strength, rng):
+    """
+    Add normal noise to mutation. Cut to clip to -100, 100. This is our search range.
+    Args:
+        population:
+        mutation_probability:
+        mutation_strength: (sigma), factor for multiplication
+
+    Returns:
+        mutated probability
+    """
     population_size = population.shape[0]
     n_features = population.shape[1]
 
@@ -8,4 +18,5 @@ def mutate(population, mutation_probability, mutation_strength, rng):
                                                     n_features).reshape(population_size, n_features) * mutation_strength
     population[mutation_probability > mutation_uniform] += normal_noise_for_mutation[
         mutation_probability > mutation_uniform]
+    population = population.clip(min=-100, max=100)
     return population

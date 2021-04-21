7572b672a539e556bedbc7ebe11ff388fa626611
Adam <narozniak.adam4@gmail.com>
2021-04-21 23:11:52 +0200

    final changes, comments, docstrings

--- src/crossovers.py
+++ src/crossovers.py
@@ -18,17 +18,4 @@ def crossover(population, crossover_probability, rng):
             temp = population[i][:genome_crossover_point]
             population[i][:genome_crossover_point] = population[population_size-i-1][:genome_crossover_point]
             population[population_size-i-1][:genome_crossover_point] = temp
-            # for j in range(0, genome_crossover_point):
-            #     swapper = population[i][j]
-            #     population[i][j] = population[population_size - i - 1][j]
-            #     population[population_size - i - 1][j] = swapper
     return population
-
-# testy, potem do usuniecia
-if __name__ == '__main__':
-    rng = np.random.default_rng()
-    population = rng.uniform(-100, 100, 5 * 4).reshape(4, 5)
-    print(population)
-    print('')
-    population = crossover(population, 1, rng)
-    print(population)
